7663dfc5f0717a1b7d3a50f0d56ecd8db1fc7fbc
Bianca-Alves <biasantosalves04@gmail.com>
2022-02-22 22:55:29 -0300

    exercício sobre update

---
+++ exercicio-funcionario/tbfuncionario.sql
@@ -0,0 +1,6 @@
+USE bdSorveteria
+
+SELECT * FROM tbfuncionario
+
+UPDATE tbfuncionario SET nomefuncionario = 'Thayani Pereira', datanascfuncionario = '16/12/1990'  WHERE codfuncionario = 1
+UPDATE tbfuncionario SET nomefuncionario = 'Junior Santos', datanascfuncionario = '11/05/1990' WHERE codfuncionario = 2
